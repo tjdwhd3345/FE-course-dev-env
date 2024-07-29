cee6adc893366b992fa328eb2bed095e840ee147
mocci <tjdwhd3345@gmail.com>
2024-07-30 00:18:18 +0900

    [webpack basic] mode, entry, output

---
+++ basic/app.js
@@ -0,0 +1,4 @@
+// const sum = require('./math.js')
+import sum from "./math"
+
+console.log(sum(2, 4))
---
+++ basic/math.js
@@ -0,0 +1,13 @@
+// CommonJS: named exports
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// module.exports = { sum }; 
+
+// CommonJS: default exports
+function sum(a, b) {
+  return a + b;
+}
+
+module.exports = sum;
