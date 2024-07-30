401c4c76ab4f770a01821196ea54cdaa39b4d6aa
mocci <tjdwhd3345@gmail.com>
2024-07-31 00:55:24 +0900

    [webpack 5 loader] webpack 5부터는 file-loader, url-loader 안써도 됨

--- webpack.config.js
+++ webpack.config.js
@@ -11,17 +11,25 @@ module.exports = {
   },
   module: {
     rules: [
+      // {
+      //   test: /\.js$/,
+      //   use: [path.resolve("./myLoader.js")],
+      // },
       {
-        test: /.js$/i,
-        use: [path.resolve("./myLoader.js")],
-      },
-      {
-        test: /.(jpg|png|gif|svg)$/i,
-        use: ["file-loader"],
-      },
-      {
-        test: /.css$/,
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset", // data URI , file 자동선택
+        generator: {
+          filename: "[name][ext]?[hash]", // file export 시 이름규칙
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 20kb // 이 값보다 작은 파일은 data URI 로 만듦
+          },
+        },
       },
     ],
   },
