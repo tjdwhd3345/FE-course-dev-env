083b8be844fe521cb3522a710c639e538639f516
mocci <tjdwhd3345@gmail.com>
2024-08-01 01:14:18 +0900

    [webpack plugin] webpack 5 부터는 플러그인 확장을 지원하지 않음

--- basic/my-webpack-plugin.js
+++ basic/my-webpack-plugin.js
@@ -1,7 +1,23 @@
 class MyWebPackPlugin {
   apply(compiler) {
-    compiler.hooks.done.tap("MyPlugin", (stats) => { 
-      console.log("MyPlugin done");
+    // 한번만 실행됨
+    // compiler.hooks.done.tap("MyPlugin", (stats) => { 
+    //   console.log("MyPlugin done");
+    // })
+    
+    compiler.hooks.emit.tapAsync("MyWebPackPlugin", (compilation, callback) => { 
+      const source = compilation.assets["main.js"].source();
+      // console.log("MyWebPackPlugin", {compilation, source})
+      // compilation.assets["main.js"].source = () => { 
+      //   const banner = [
+      //     "/**",
+      //     " * MyWebPackPlugin 적용 결과",
+      //     " * Build Date: 2024-07-31",
+      //     " */"
+      //   ].join("\n");
+      //   return banner + "\n\n" + source;
+      // }
+      callback();
     })
   }
 }
