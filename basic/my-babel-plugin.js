18385662bf6cce5cb0b16bc24a4b362f921c4bd2
mocci <tjdwhd3345@gmail.com>
2024-08-02 23:22:24 +0900

    [babel] custom plugin으로 변수타입 변환

--- basic/my-babel-plugin.js
+++ basic/my-babel-plugin.js
@@ -1,14 +1,23 @@
 module.exports = function MyBabelPlugin() {
   return {
     visitor: {
-      Identifier(path) {
-        const name = path.node.name;
-        console.log("MyBabelPlugin Identifier path.node.name", { name })
-        // reverse the name: JavaScript -> tpircSavaJ
-        path.node.name = name
-          .split("")
-          .reverse()
-          .join("");
+      // Identifier(path) {
+      //   const name = path.node.name;
+      //   console.log("MyBabelPlugin Identifier path.node.name", { name })
+      //   // reverse the name: JavaScript -> tpircSavaJ
+      //   path.node.name = name
+      //     .split("")
+      //     .reverse()
+      //     .join("");
+      // },
+      VariableDeclaration(path) {
+        const kind = path.node.kind;
+        console.log("VariableDeclaration", { kind });
+
+        // const 변수를 var로 변환
+        if (kind === "const") {
+          path.node.kind = "var";
+        }
       },
     },
   };
