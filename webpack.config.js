30a088f8da1b3c40721635fe7ba06964a0aae0c6
mocci <tjdwhd3345@gmail.com>
2024-08-02 22:27:24 +0900

    [webpack plugin] 전체 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,42 @@
 const path = require("path");
+const {BannerPlugin} = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
   entry: {
-    main: "./src/app.js"
+    main: "./src/app.js",
   },
   output: {
-    filename: "[name].js",
-    path: path.resolve("./dist")
+    filename: "[name].js?[hash]",
+    path: path.resolve("./dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          // "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
       },
       {
-        test: /\.(png|jpg|svg|gif)$/,
-        loader: "url-loader",
-        options: {
-          name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
-        }
-      }
-    ]
-  }
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: "asset",
+        generator: {
+          filename: "[name][ext]?[hash]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024,
+          },
+        },
+      },
+    ],
+  },
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
@@ -32,4 +44,15 @@ module.exports = {
    * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
    * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
    */
+  plugins: [
+    new BannerPlugin({banner: `Build: ${new Date().toLocaleString()}`}),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      templateParameters: {
+        env: process.env.NODE_ENV,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: "[name].css?[hash]"}),
+  ],
 };
